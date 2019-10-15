d513f390862949efcea2096844acb12ff99ebe1c
ksshin <sinsinsin01@gmail.com>
2019-10-15 21:51:37 +0900

    version 6

--- catkin_ws/src/kss_opencv/node/opencv_data_get.py
+++ catkin_ws/src/kss_opencv/node/opencv_data_get.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python
 
-import rospy
-from darknet_ros_msgs.msg import BoundingBoxes
-import numpy as np
 import cv2
-from cv_bridge import CvBridge, CvBridgeError
+import rospy
+import numpy as np
+
 from sensor_msgs.msg import Image
+from matplotlib import pyplot as plt
+from cv_bridge import CvBridge, CvBridgeError
+from darknet_ros_msgs.msg import BoundingBoxes
+
+
+#check flag
+
+
 
 
 #color set
+blue_color = (255,0,0)
+green_color = (0,255,0)
 red_color = (0,0,255)
+white_color = (255,255,255)
+black_color = (0,0,0)
 
 
 class darknet:
@@ -52,34 +63,58 @@ class darknet:
 
 
 
+        print("normal",self.x_min,"length : ",len(self.x_min))
         for i in range(0,len(self.x_min),1):
-            print("only person {}".format(i))
-            print(self.x_min[i])
-            print(self.y_min[i])
-            print(self.x_max[i])
-            print(self.y_max[i])
+            try :
+                print("only person {}".format(i))
+                print(self.x_min[i])
+                print(self.y_min[i])
+                print(self.x_max[i])
+                print(self.y_max[i])
 
-            mid_x = (self.x_min[i] + self.x_max[i])/2 - 40
-            mid_y = (self.y_min[i] + self.y_max[i])/2
+                mid_x = (self.x_min[i] + self.x_max[i])/2 - 40
+                mid_y = (self.y_min[i] + self.y_max[i])/2
 
+                height, width, channels = cv_image.shape
 
+                #point center
+                dis_x_center = 320
+                dis_y_center = 240
 
-            print("mid values")
-            print(mid_x)
-            print(mid_y)
+                #circle center
+                upper_left = height / 2
+                bottom_right = width / 2
 
-            cv2.putText(cv_image,'person', (int(mid_x), int(mid_y)), cv2.FONT_HERSHEY_SIMPLEX,0.7,(255, 255, 255),2)
-            cv_image = cv2.rectangle(cv_image,(self.x_min[i],self.y_min[i]),(self.x_max[i],self.y_max[i]),red_color,2)
+                #print("mid values")
+                #print(mid_x)
+                #print(mid_y)
+                #print(upper_left)
+                #print(bottom_right)
 
-            cv2.imshow('window', cv2.resize(cv_image, (800, 450)))
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                cv2.destroyAllWindows()
-                exit()
+                #text detect image name
+                cv2.putText(cv_image, 'person', (int(mid_x), int(mid_y)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+
+                #draw circle
+                #cv_image = cv2.circle(cv_image,(bottom_right,upper_left),1,white_color,1)
+
+                cv_image = cv2.line(cv_image, (dis_x_center, dis_y_center), (dis_x_center, dis_y_center), red_color, 5)
+
+                #draw rectangle
+                cv_image = cv2.rectangle(cv_image,(self.x_min[i],self.y_min[i]),(self.x_max[i],self.y_max[i]),red_color,2)
+
+                cv2.imshow('window', cv2.resize(cv_image, (800, 450)))
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    cv2.destroyAllWindows()
+                    break
+            except IndexError as e :
+                print("ERROR",self.x_min,"length : ",len(self.x_min))
+                print(e)
+
 
 
 
     def sub_opencv_img(self):
-        self._sub = rospy.Subscriber('raspicam_node/image_raw', Image, self.callback_opencv, queue_size=1)
+        self.sub = rospy.Subscriber('raspicam_node/image_raw', Image, self.callback_opencv, queue_size=1)
 
 
 if __name__ == '__main__':
